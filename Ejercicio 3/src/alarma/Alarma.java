8c20cb6bf40d955a444093e6d3cae05a951e9333
John Mora <johmora12@engineer.com>
2016-10-14 08:48:48 -0600

    Agregado Ejercicio 3

---
+++ Ejercicio 3/src/alarma/Alarma.java
@@ -0,0 +1,17 @@
+package alarma;
+
+import alarma.Timbre;
+
+public class Alarma extends Timbre {
+	public void m1() {
+		System.out.println("alarma 1");
+	}
+	 
+	public void m2() {
+		super.m1();
+	}
+	 
+	public String toString() {
+		return super.toString() + "..." + super.toString();
+	}
+}
